441ebe6ed81e72144e26b2cae091f347f15a874a
MV-max <voronkov.maksik@mail.ru>
2023-02-04 21:58:14 +0300

    Сделал калибровку и движение по любой линии

--- Line/Line.ino
+++ Line/Line.ino
@@ -10,6 +10,8 @@
 
 #define SPEED 100
 
+bool type_of_line = false; // true - black line, false - white line
+
 void setup() {
   pinMode(BACK_LEFT_MOTOR, OUTPUT);
   pinMode(LEFT_MOTOR, OUTPUT);
@@ -20,11 +22,13 @@ void setup() {
   pinMode(RIGHT_CENTRAL_SENSOR, INPUT);
   pinMode(LEFT_CENTRAL_SENSOR, INPUT);
   Serial.begin(9600);
+  if(analogRead(RIGHT_CENTRAL_SENSOR) > analogRead(RIGHT_SENSOR)) type_of_line = true;
+  else type_of_line = false;
 }
 
 
 void loop() {
-  line(100,0);
+  line(100, 0);
 }
 
 
@@ -43,34 +47,52 @@ void line(int max_speed, int min_speed) {
   int right_sensor = analogRead(RIGHT_SENSOR);
 
 
-  max_color = max(max_color,left_central_sensor);
-  max_color = max(max_color,right_central_sensor);
-  max_color = max(max_color,left_sensor);
-  max_color = max(max_color,right_sensor);
+  max_color = max(max_color, left_central_sensor);
+  max_color = max(max_color, right_central_sensor);
+  max_color = max(max_color, left_sensor);
+  max_color = max(max_color, right_sensor);
 
-  min_color = min(min_color,left_central_sensor);
-  min_color = min(min_color,right_central_sensor);
-  min_color = min(min_color,left_sensor);
-  min_color = min(min_color,right_sensor);
+  min_color = min(min_color, left_central_sensor);
+  min_color = min(min_color, right_central_sensor);
+  min_color = min(min_color, left_sensor);
+  min_color = min(min_color, right_sensor);
 
   int middle_color = min_color + (0.4 * (max_color - min_color));
 
-  if (left_central_sensor < middle_color and right_central_sensor < middle_color) {
-    if ((old_left_central_sensor > middle_color and old_right_central_sensor < middle_color) or (left_sensor > middle_color or old_left_sensor > middle_color)) {
-      right_speed = max_speed;
-      left_speed = min_speed;
-    } else if ((old_left_central_sensor < middle_color and old_right_central_sensor > middle_color) or (right_sensor > middle_color or old_right_sensor > middle_color)) {
-      right_speed = min_speed;
-      left_speed = max_speed;
+  if ((left_central_sensor > middle_color and right_central_sensor > middle_color and !type_of_line) or (left_central_sensor < middle_color and right_central_sensor < middle_color and type_of_line)) {
+    if (old_left_central_sensor > middle_color and old_right_central_sensor < middle_color) {
+      if (type_of_line) {
+        right_speed = max_speed;
+        left_speed = min_speed;
+      }
+      else {
+        right_speed = min_speed;
+        left_speed = max_speed;
+      }
+    } else if (old_left_central_sensor < middle_color and old_right_central_sensor > middle_color) {
+      if (type_of_line) {
+        right_speed = min_speed;
+        left_speed = max_speed;
+      }
+      else {
+        right_speed = max_speed;
+        left_speed = min_speed;
+      }
+
     } else {
       ;
       // right_speed = max_speed;
       // left_speed = max_speed;
     }
   } else {
-
-    left_speed = map(right_central_sensor, min_color, max_color, min_speed, max_speed);
-    right_speed = map(left_central_sensor, min_color, max_color, min_speed, max_speed);
+    if (type_of_line) {
+      left_speed = map(right_central_sensor, min_color, max_color, min_speed, max_speed);
+      right_speed = map(left_central_sensor, min_color, max_color, min_speed, max_speed);
+    }
+    else {
+      left_speed = map(right_central_sensor, max_color, min_color, min_speed, max_speed);
+      right_speed = map(left_central_sensor, max_color, min_color, min_speed, max_speed);
+    }
     old_right_central_sensor = right_central_sensor;
     old_left_central_sensor = left_central_sensor;
     old_left_sensor = left_sensor;
