d0d1338ecaafafffc93911bfa71535d7c931d2c5
chebur5581 <110908198+chebur5581@users.noreply.github.com>
2023-03-04 12:37:51 +0300

    Add files via upload

--- Server/server.py
+++ Server/server.py
@@ -1,12 +1,20 @@
 import socket
 from settings import *
+from buffer import Buffer
 
 
+# TODO: добавить список с актуальными потоками, при запуске еспшка будет отправлять соответствующее сообщение
+#  о включении, на сервер.
+#  Еспшка будет отправлять данные на сервер которые он будет буферизировать(нужно подобрать оптимальный его размер).
+#  Пользователь по запросу [check] будет получать список актуальных потоков.
+#  При помощи [get data] пользователь будет получать данные из буфера
 class Server():
     def __init__(self, webServ=('localhost', 80), page_dir='pages', listen=4):
         self.Whost, self.Wport = webServ
         self.page_dir = page_dir
         self.listen = listen
+
+        self.buff = Buffer(buffer_size=10)
 
         self.WebServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.WebServer.bind((self.Whost, self.Wport))
@@ -19,35 +27,38 @@ class Server():
         self.log(f'Web server successful started on {self.Whost}:{self.Wport}', 'success')
         try:
             while True:
-                Wclient_socket, Waddres = self.WebServer.accept()
-                Wdata = Wclient_socket.recv(1024).decode('utf-8')
-                print('/>', Wdata)
-                if Wdata.split(' ')[0] == '[data]':
-                    print('ok')
-                    # do something with the data
-                    continue
-                else:
-                    content = self.load_page(Wdata)
+                self.Wclient_socket, self.Waddres = self.WebServer.accept()
+                Wdata = self.Wclient_socket.recv(1024).decode('utf-8')
 
-                    Wclient_socket.send(content)
-                    Wclient_socket.shutdown(socket.SHUT_WR)
+                self.data_handler(Wdata)
+
+
         except KeyboardInterrupt:
             self.log('Server shutdown', 'warning')
             self.WebServer.close()
 
-    def data_handler(self, data):
-        pass
+    def data_handler(self, data=str):
+        start_from = data.split(' ')[0]
+        if start_from == 'GET':
+            content = self.load_page(data)
+
+            self.Wclient_socket.send(content)
+            self.Wclient_socket.shutdown(socket.SHUT_WR)
+
+        if start_from == '[data]':
+            print(data)
 
     def load_page(self, request):
         responce = ''
 
+        print(request)
         path = request.split(' ')[1]
 
         try:
             with open('pages' + path, 'rb') as file:
                 responce = file.read()
             return HDRS.encode('utf-8') + responce
-        except PermissionError or FileNotFoundError:
+        except FileNotFoundError or OSError:
             self.log('user request not existing page', 'warning')
             return (HDRS_404 + 'page not exist)').encode('utf-8')
 
